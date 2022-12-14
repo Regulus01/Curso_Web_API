0e598f0a1e715c3f720b3b3eb95e90e3d39cc940
JosePc <jose.csousa22@gmail.com>
2022-12-13 21:11:46 -0300

    feat: Adiciona API

---
+++ VemDeZap.Domain/Commands/Usuario/AdicionarUsuario/Notifications/AvisarAdiministradores.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System.Diagnostics;
+
+namespace VemDeZap.Domain.Commands.Usuario.AdicionarUsuario.Notifications
+{
+    public class AvisarAdiministradores : INotificationHandler<AdicionarUsuarioNotification>
+    {
+        public async Task Handle(AdicionarUsuarioNotification notification, CancellationToken cancellationToken)
+        {
+            Debug.WriteLine("Chamar o webservice que avisa que novo usuário se cadastrou: " + notification.Usuario.PrimeiroNome);
+        }
+    }
+}
